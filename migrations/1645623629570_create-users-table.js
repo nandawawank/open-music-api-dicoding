1a384b6ed3a16e0e7df6e6cd40088ba698bc5ec6
Nanda Wawan K <nandawawank@gmail.com>
2022-02-23 21:26:30 +0700

    create handler, service, validate and migration for user scope

---
+++ migrations/1645623629570_create-users-table.js
@@ -0,0 +1,41 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'VARCHAR(20)',
+      notNull: true,
+    },
+    password: {
+      type: 'VARCHAR(100)',
+      notNull: true,
+    },
+    fullname: {
+      type: 'VARCHAR(100)',
+    },
+    token: {
+      type: 'TEXT',
+      default: null,
+    },
+    refresh_token: {
+      type: 'TEXT',
+      default: null,
+    },
+    create_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+    },
+    update_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
